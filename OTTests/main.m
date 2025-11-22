9d23b6f865b4bb82846aefb56c481940fcd3c8bd
Manfred Bergmann <mb@software-by-mabe.com>
2025-11-22 11:09:11 +0100

    OTTests now run.

---
+++ OTTests/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  OTTests
+//
+//  Created by Manfred Bergmann on 20.11.25.
+//
+
+#import <ObjFW/ObjFW.h>
+
+int main_dummy(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        [OFStdOut writeString:@"Hello, World!"];
+    }
+    return EXIT_SUCCESS;
+}
