cfe6a8ea521e1375a448eac6fed8d3eca2ce7509
Manfred Bergmann <mb@software-by-mabe.com>
2025-11-23 12:43:40 +0100

    Added OTTest app with embedded ObjCLogger framework embedded.

---
+++ OTTestApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  OTTestApp
+//
+//  Created by Manfred Bergmann on 23.11.25.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main_dummy(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
